0fd7c237f1174845269871e98a1b821631d07ffa
Luth-Badila <FrancescoLutfi10@gmail.com>
2023-08-28 12:06:08 +0700

    added global style, image and font

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import Logo from "../../../public/logo.png";
 
 function Navbar() {
   return (
     <nav>
+      <Image src={Logo} alt="Logo" width={30} quality={100} placeholder="blur" />
       <h1>My Tickets</h1>
       <Link href="/">Dashboard</Link>
       <Link href="/tickets">Tickets</Link>
