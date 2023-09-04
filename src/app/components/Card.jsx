85a8bd7e0beb35a5c7ebda32a27fcb4564a7e965
Luth-Badila <FrancescoLutfi10@gmail.com>
2023-09-04 19:33:29 +0700

    changed content in card

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -7,13 +7,13 @@ export default function Card({ caption, title, icon }) {
   return (
     <>
       <div className="group">
-        <div className="relative grid w-full lg:h-[20vh] h-[25vh] flex-col items-start justify-start overflow-hidden rounded-xl bg-white px-2">
+        <div className="relative grid w-full lg:h-[20vh] h-[25vh] flex-col overflow-hidden rounded-xl bg-green-700 px-2">
           <div className="absolute inset-0 m-0 h-full full overflow-hidden rounded-none  shadow-none">
-            <div className="flex flex-col justify-center items-center lg:mt-0 mt-[30px]">
-              <Icon icon={icon} width="120" height="80" color="#DAA520" />
+            <div className="flex flex-col justify-center items-center lg:mt-3 mt-[30px]">
+              <Icon icon={icon} width="120" height="80" color="white" />
               <h2 className="mb-6 block font-sans text-xl font-medium leading-[1.5] antialiased">{title}</h2>
             </div>
-            <div className="to-bg-black-10 absolute group-hover:top-0 transition-all duration-300 top-full h-full w-full bg-gradient-to-b from-[#F1F9FCFF] via-[#F0F8FBFF]"></div>
+            <div className="to-bg-black-10 absolute group-hover:top-0 transition-all duration-300 top-full h-full w-full bg-gradient-to-b from-[#d6fed7] via-[#7bfe6f]"></div>
           </div>
           <div className="relative top-full group-hover:top-0 transition-all duration-300 ">
             <p className="text-md block font-sans font-medium leading-[1.5] antialiased">{caption}</p>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,26 +1,28 @@
 import HomePage from "./components/Home";
+import Gallery from "./components/Gallery";
+import Card from "./components/Card";
 export default function Home() {
   const items = [
     {
       image: "https://images.pexels.com/photos/17588450/pexels-photo-17588450/free-photo-of-noir-et-blanc-mode-homme-gens.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
       caption: "Kami membantu anda untuk menerjemahkan teks baik untuk tugas sekolah, kuliah, penelitian maupun pekerjaan anda",
       instagramUrl: "instagram url here",
-      title: "Translation",
-      icon: "bx:edit",
+      title: "Sablon Kaos",
+      icon: "la:tshirt",
     },
     {
       image: "https://images.pexels.com/photos/17640285/pexels-photo-17640285/free-photo-of-lumineux-mode-gens-femme.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
       caption: "Kami membantu anda mengkoreksi tulisan anda agar tidak ada typo dan sesuai dengan EYD ata Grammar",
       instagramUrl: "instagram url here",
-      title: "Proofread",
-      icon: "ph:read-cv-logo-bold",
+      title: "Sablon Banner",
+      icon: "ph:flag-banner",
     },
     {
       image: "https://images.pexels.com/photos/17640285/pexels-photo-17640285/free-photo-of-lumineux-mode-gens-femme.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
       caption: "Kami membantu anda merubah tulisan agar sesuai dengan EYD atau Grammar",
       instagramUrl: "instagram url here",
-      title: "Editing",
-      icon: "ant-design:translation-outlined",
+      title: "Satuan maupun lusinan",
+      icon: "bi:repeat-1",
     },
   ];
   const accordionData = [
@@ -51,9 +53,66 @@ export default function Home() {
       ),
     },
   ];
+  const galleryTab = [
+    // you can add more image if you want
+    {
+      imageUrl: "https://themewagon.github.io/snapshot/images/model-1.jpg",
+      type: "Nature",
+      title: "Beautiful Work",
+    },
+    {
+      imageUrl: "https://themewagon.github.io/snapshot/images/model-8.jpg",
+      type: "Nature",
+      title: "Beautiful Work",
+    },
+    {
+      imageUrl: "https://themewagon.github.io/snapshot/images/model-3.jpg",
+      type: "Nature",
+      title: "Beautiful Work",
+    },
+    {
+      imageUrl: "https://themewagon.github.io/snapshot/images/model-4.jpg",
+      type: "Nature",
+      title: "Beautiful Work",
+    },
+    {
+      imageUrl: "https://themewagon.github.io/snapshot/images/model-5.jpg",
+      type: "Nature",
+      title: "Beautiful Work",
+    },
+  ];
+
+  const slides = galleryTab.map((item) => ({
+    src: item.imageUrl,
+    width: 3840,
+    height: 2560,
+    srcSet: [
+      { src: item.imageUrl, width: 320, height: 213 },
+      { src: item.imageUrl, width: 640, height: 426 },
+      { src: item.imageUrl, width: 1200, height: 800 },
+      { src: item.imageUrl, width: 2048, height: 1365 },
+      { src: item.imageUrl, width: 3840, height: 2560 },
+    ],
+  }));
   return (
     <main>
-      <HomePage />
+      <section>
+        <HomePage />
+      </section>
+
+      <section>
+        <div className="px-2 mx-auto flex justify-center flex-col gap-3 my-16">
+          <div className="container mx-auto grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6">
+            {items.map(({ caption, image, instagramUrl, title, icon }, index) => {
+              return <Card key={index} caption={caption} image={image} instagramUrl={instagramUrl} title={title} icon={icon} />;
+            })}
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <Gallery />
+      </section>
     </main>
   );
 }
